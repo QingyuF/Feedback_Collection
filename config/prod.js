6193a1f66ecccf077cea2fadd4ba11dfec9268ea
Qingyu Fan <fanqingyustudy@gmail.com>
2022-08-26 16:40:04 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,16 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+
+
+/*
+    googleClientID: '824964938132-qp035381mlscl25r940afrlsc63u1tnk.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://QYF:3CQVByZJRK1PMNKw@cluster0.jbteru4.mongodb.net/emailyprod?retryWrites=true&w=majority',
+
+
+*/
---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    googleId: String
+        
+});
+
+mongoose.model('users', userSchema);
+
+
+
+
+
+
+
