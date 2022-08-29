046703a4a3ed50c702c951f8d10f9f00597674a5
Qingyu Fan <fanqingyustudy@gmail.com>
2022-08-28 22:03:40 -0500

    Add billing and client

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div style={{textAlign:'center', backgroundColor: 'rgb(0,0,0,0.3)'}}>
+            <h1>
+                Emaily!
+            </h1>
+            Collect feedback from your users
+        </div>
+    );
+};
+
+
+export default Landing;
